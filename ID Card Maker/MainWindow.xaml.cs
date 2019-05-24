c3c6f9180904aa74728d1ca0a39e573d78894045
Dan Leonard <daniel@danleonard.us>
2019-05-24 13:37:39 -0500

    tweaks of printing process

--- ID Card Maker/MainWindow.xaml.cs
+++ ID Card Maker/MainWindow.xaml.cs
@@ -48,7 +48,7 @@ namespace ID_Card_Maker
         {
             windowDesign = new Window()
             {
-                Title = "Print Preview",
+                //Title = "Print Preview",
                 SizeToContent = SizeToContent.WidthAndHeight,
                 Content = cardPreviewer,
                 ResizeMode = ResizeMode.NoResize,
@@ -61,6 +61,7 @@ namespace ID_Card_Maker
         /// </summary>
         private void InvokePrint(object sender, RoutedEventArgs e)
         {
+            /*
             // Create a print dialog object
             PrintDialog dialog = new PrintDialog();
             dialog.UserPageRangeEnabled = true;
@@ -68,8 +69,12 @@ namespace ID_Card_Maker
             if (dialog.ShowDialog() == true)
             {
                 // Print the card design
-                dialog.PrintVisual(cardPreviewer, "ID Card");
+                //dialog.PrintVisual(cardPreviewer, "ID Card");
+                PrintHelper.ShowPrintPreview(PrintHelper.GetFixedDocument(cardPreviewer, dialog));
             }
+            */
+
+            Print(cardPreviewer);
         }
 
         /// <summary>
@@ -103,7 +108,50 @@ namespace ID_Card_Maker
         {
             ((TextBox)sender).SelectAll();
         }
+
+        /// <summary>
+        /// Method for safe printing all on one page
+        /// </summary>
+        /// <param name="v">Visual to be printed</param>
+        private void Print(Visual v)
+        {
+
+            System.Windows.FrameworkElement e = v as System.Windows.FrameworkElement;
+            if (e == null)
+                return;
+
+            PrintDialog pd = new PrintDialog();
+            if (pd.ShowDialog() == true)
+            {
+                //store original scale
+                Transform originalScale = e.LayoutTransform;
+                //get selected printer capabilities
+                System.Printing.PrintCapabilities capabilities = pd.PrintQueue.GetPrintCapabilities(pd.PrintTicket);
+
+                //get scale of the print wrt to screen of WPF visual
+                double scale = Math.Min(capabilities.PageImageableArea.ExtentWidth / e.ActualWidth, capabilities.PageImageableArea.ExtentHeight /
+                               e.ActualHeight);
+
+                //Transform the Visual to scale
+                e.LayoutTransform = new ScaleTransform(scale, scale);
+
+                //get the size of the printer page
+                System.Windows.Size sz = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
+
+                //update the layout of the visual to the printer page size.
+                e.Measure(sz);
+                e.Arrange(new System.Windows.Rect(new System.Windows.Point(capabilities.PageImageableArea.OriginWidth, capabilities.PageImageableArea.OriginHeight), sz));
+
+                //now print the visual to printer to fit on the one page.
+                pd.PrintVisual(v, "My Print");
+
+                //apply the original transform.
+                e.LayoutTransform = originalScale;
+            }
+        }
     }
+
+
 
     public class Bio
     {
---
+++ ID Card Maker/PrintHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace ID_Card_Maker
+{
+    /// <summary>
+    /// Helper class for safe printing
+    /// </summary>
+    /// <remarks>
+    /// Thanks to user DermFrench on StackOverflow for posting this code
+    /// </remarks>
+    /// <see>
+    /// https://stackoverflow.com/a/19554234
+    /// </see>
+    public static class PrintHelper
+    {
+
+        public static FixedDocument GetFixedDocument(FrameworkElement toPrint, PrintDialog printDialog)
+        {
+            var capabilities = printDialog.PrintQueue.GetPrintCapabilities(printDialog.PrintTicket);
+            var pageSize = new System.Windows.Size(printDialog.PrintableAreaWidth, printDialog.PrintableAreaHeight);
+            var visibleSize = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
+            var fixedDoc = new FixedDocument();
+            //If the toPrint visual is not displayed on screen we neeed to measure and arrange it  
+            toPrint.Measure(new System.Windows.Size(double.PositiveInfinity, double.PositiveInfinity));
+            toPrint.Arrange(new Rect(new System.Windows.Point(0, 0), toPrint.DesiredSize));
+            //  
+            var size = toPrint.DesiredSize;
+            //Will assume for simplicity the control fits horizontally on the page  
+            double yOffset = 0;
+            while (yOffset < size.Height)
+            {
+                var vb = new VisualBrush(toPrint)
+                {
+                    Stretch = Stretch.None,
+                    AlignmentX = AlignmentX.Left,
+                    AlignmentY = AlignmentY.Top,
+                    ViewboxUnits = BrushMappingMode.Absolute,
+                    TileMode = TileMode.None,
+                    Viewbox = new Rect(0, yOffset, visibleSize.Width, visibleSize.Height)
+                };
+                var pageContent = new PageContent();
+                var page = new FixedPage();
+                ((IAddChild)pageContent).AddChild(page);
+                fixedDoc.Pages.Add(pageContent);
+                page.Width = pageSize.Width;
+                page.Height = pageSize.Height;
+                var canvas = new Canvas();
+                FixedPage.SetLeft(canvas, capabilities.PageImageableArea.OriginWidth);
+                FixedPage.SetTop(canvas, capabilities.PageImageableArea.OriginHeight);
+                canvas.Width = visibleSize.Width;
+                canvas.Height = visibleSize.Height;
+                canvas.Background = vb;
+                page.Children.Add(canvas);
+                yOffset += visibleSize.Height;
+            }
+            return fixedDoc;
+        }
+
+        public static void ShowPrintPreview(FixedDocument fixedDoc)
+        {
+            var wnd = new Window();
+            var viewer = new DocumentViewer();
+            viewer.Document = fixedDoc;
+            wnd.Content = viewer;
+            wnd.ShowDialog();
+        }
+
+        public static void PrintNoPreview(PrintDialog printDialog, FixedDocument fixedDoc)
+        {
+            printDialog.PrintDocument(fixedDoc.DocumentPaginator, "Test Print No Preview");
+
+        }
+
+    }
+}
